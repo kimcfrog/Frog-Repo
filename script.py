fd176907bf7f4000f226f565ef9020262c76404e
Kim Ceballos <kimberlyceballos13@gmail.com>
2022-09-02 18:54:32 -0700

    init commit

---
+++ script.py
@@ -0,0 +1,21 @@
+import sys
+import requests
+import os
+import math
+
+# print(sys.version)
+# print(sys.executable)
+
+
+# def greet(who_to_greet):
+#     greeting = f"Hello {who_to_greet}"
+#     return greeting
+
+
+# # print(greet("World!"))
+# # print(greet("Froggy"))
+# r = requests.get("https://coreyms.com")
+# print(r.status_code)
+
+# name = input("Enter your name: ")
+# print(f"Hello, {name}")
